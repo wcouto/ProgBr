f8ef896f88ca1d5d57f7ea574086d785febab3d2
Willian Couto <willian_994@hotmail.com>
2023-02-02 09:37:36 -0300

    Att backup

---
+++ React/react-prog-br/src/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+export default function Header() {
+    return (
+        <header className='header'>
+        <h1>ProgBr</h1>
+        <ul className='menu'>
+            <li>About</li>
+            <li>Buy</li>
+            <li>Contact</li>
+        </ul>
+        </header>
+    )
+}
