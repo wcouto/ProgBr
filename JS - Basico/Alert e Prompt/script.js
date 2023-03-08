37dc20b2014308e57e6a2b5202bce35596c9182c
Willian Couto <willian.couto@vegamonitoramento.com.br>
2023-03-08 15:22:09 -0300

    Alert, Promp, Strint to Number

---
+++ JS - Basico/Alert e Prompt/script.js
@@ -0,0 +1,13 @@
+var nome = prompt("Qual o seu nome?");
+
+alert("Bom dia, " + nome);
+
+var numero = prompt("Digite um numero");
+
+alert("O numero que voce digitou é " + numero);
+
+var numero = parseInt(prompt("Digite outro numero"));
+
+var dobro = numero + numero;
+
+alert("O dobro de " + numero + " é " + dobro);
